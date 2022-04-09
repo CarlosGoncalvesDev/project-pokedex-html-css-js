1e9aee362ed113a309e12405d0e51b93e1b74b1b
CarlosGoncalvesDev <carloseduardo05.dev@gmail.com>
2022-04-09 12:32:20 -0300

    Criação do Projeto

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+        const idPokemonSelecionado = pokemon.attributes.id.value
+       
+        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const PokemonAtivoNaListagem = document.querySelector('.ativo')
+        PokemonAtivoNaListagem.classList.remove('ativo')
+        
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
